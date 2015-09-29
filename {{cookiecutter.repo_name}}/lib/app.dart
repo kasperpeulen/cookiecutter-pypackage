42807dd796299b173eda4f88ac374df274dc4376
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-29 18:37:49 +0200

    fix structure

--- {{cookiecutter.repo_name}}/lib/app.dart
+++ {{cookiecutter.repo_name}}/lib/app.dart
@@ -1,8 +1,7 @@
 library {{cookiecutter.package_name}}.app;
 
 import 'package:angular2/angular2.dart';
-import 'components/hello_world/hello_world.dart';
 
 @Component(selector: 'app')
-@View(templateUrl: 'app.html', directives: const [HelloWorld])
+@View(templateUrl: 'app.html', directives: const [])
 class App {}
